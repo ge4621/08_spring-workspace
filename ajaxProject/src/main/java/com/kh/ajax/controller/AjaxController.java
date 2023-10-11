2a192ff6458b46283137777acb8c7f6fcd256af8
(윤지영) <ge4621@naver.com>
2023-10-11 15:33:31 +0900

    ..
    ..

--- ajaxProject/src/main/java/com/kh/ajax/controller/AjaxController.java
+++ ajaxProject/src/main/java/com/kh/ajax/controller/AjaxController.java
@@ -1,12 +1,19 @@
 package com.kh.ajax.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.kh.ajax.model.vo.Member;
 
 @Controller
 public class AjaxController {
@@ -42,12 +49,105 @@ public class AjaxController {
 	 * 따라서 필터 사용해도 글자가 깨짐 => 해결 법 : produces사용
 	 * (속성값 추가 하기 위해서는 매핑값 앞에 value를 붙여준다.)
 	 */
+	/*
 	@ResponseBody
 	@RequestMapping(value="ajax1.do",produces = "text/html;charset=UTF-8")
 	public String ajaxMethod1(String name, int age) {
 		String responseDate="응답문자열 : " + name + "은(는)" + age + "살 입니다.";
 		return responseDate;
 		
+	}
+	*/
+	
+	/*
+	//다수의 응답데이터가 있을 경우???
+	@RequestMapping("ajax1.do")
+	public void ajaxMethod1(String name, int age, HttpServletResponse response) throws IOException {
+	//요청 처리가 다 됐다는 가정하에 데이터 응답
+		
+		
+		//response.setContentType("text/html;charset=UTF-8");
+		//response.getWriter().print(name);
+		//response.getWriter().print(age);
+		//연이어서 출력하는 데이터가 하나의 문자열로 연이어져 있음(값은 깨지지 않고 잘 넘어감)
+		
+		
+		//JSON(JavaScript Object Notation) 형태로 담아서 응답
+		//JSONArray => [값,값,값,......] => 자바에서의 ArrayList와 유사 (list에 추가할 때는 add)
+		//JSONObject => {키:값, 키:값}  => 객체(객체여서 인덱스 개념이 없어 순서가 없다. 그렇기 때문에 {키:값}을 사용 => 자바에서의 HashMap과 유사 (map에 추가 할때는 put)
+		
+		//첫번째 방법 JSONArray로 담아서 응답
+		
+		//JSONArray jArr = new JSONArray(); //[] =>현재는 빈 배열형(=처음 만들었을 때의 형태)
+		//jArr.add(name); //[] -> ["차은우"] => 이름을 추가했기 때문에 입력값 이름이 들어간다.
+		//jArr.add(age); // ["차은우"] -> ["차은우",20] => 이름이 들어가 있는 배열에 나이를 입력했기 때문에 ["차은우"] 뒤에 나이 값이 추가 되어 ["차은우", 20] 이 된다. => 인덱스를 이용해서 하나씩 뽑을 수 있다.
+		
+		
+		//두번째 방법. JSONObject로 담아서 응답
+		//JSONObject jObj = new JSONObject();  //=> {} =>처음 만들었을 때의 형태 , HashMap는 순서가 없다.(주의 해야 할 점)
+		//jObj.put("name", name); //앞이 키값, 뒤는 앞에서 받아온 벨류 값 {name:'차은우'} => 앞에서 차은우를 입력하면 담겨지는 형태
+		//jObj.put("age", age); //{name:'차은우', age:10} => String형태로 담길때는 ''사용,int 형으로 담길때는 그냥 담김 => {name:'차은우'}가 담겨 있는 곳에 나이를 입력 받았기 때문에 {name:'차은우', int:10}으로 됨
+								//Object자료형이기 때문에 순서가 없다.(입력한 순서가 유기자 되지 않는다. 순서를 사용 하고 싶다면 JSONArray를 사용해야 한다.)
+		
+		response.setContentType("application/json; charset=UTF-8");
+		response.getWriter().print(jObj);
+	
+	}
+	 */
+	/*(ajax1.do가 value값이고 괄호안의 값이 1개일 경우 ajax1.do으로만 사용 가능 )*/
+	/*
+	@ResponseBody
+	@RequestMapping(value="ajax1.do", produces = "application/json; charset=UTF-8")
+	public String ajaxMethod1(String name, int age) {
+		
+		JSONObject jObj = new JSONObject(); //{} => 처음 형태
+		jObj.put("name", name); //{} => {name:'차은우'} 변경
+		jObj.put("age", age); //{name:'차은우'} => {name:'차은우',age:10} 변경
+		
+		return jObj.toJSONString(); //return jObj만 쓸 경우 자료형이 맞지 않음 따라서 String로 바꿔줘야 됨 => .toJSONString()사용 => "{name:'차은우',age:10}" 형태(String)로 넘어감, 반환될때는 String 받을 때는 {} 형태
+	
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="ajax2.do",produces = "application/json; charset=UTF-8")
+	public String ajaxMethod2(int num) {
+		
+		//Member m = mService.selectMember(num); //일반적인 방법 but 지금 아무것도 없기 때문에 member안에 값이 담겨 있다고 가정
+		Member m = new Member("user01","pass01","차은우",20,"01011112222"); //더미(원래는 위의 코드 사용 해야 한는데...데이터가 없으니 가정해서 진행)
+		
+		//JSON형태로 만들어서 응답해야지 반환가능 => toJSONString() 사용해서 반환
+		JSONObject jObj = new JSONObject();  //{} => 초기 형태
+		jObj.put("userId", m.getUserId());	//{} => {userId:"user01"}
+		jObj.put("userName", m.getUserName());	//{userId:"user01"} => {userId:"user01",userName:"차은우"}
+		jObj.put("age", m.getAge()); //반환형 int 	//{userId:"user01",userName:"차은우"} => {userId:"user01",userName:"차은우",age:10}
+		jObj.put("phone", m.getPhone());//{userId:"user01",userName:"차은우",age:10} => {userId:"user01",userName:"차은우",age:10,phone:"01011111111"}  JSONObject는 순서가 보장되지 않는다. => 입력한 순서대로 값이 안나올 수 있다.
+		
+		return jObj.toJSONString();  //그냥 m을 하면 반환형도 안맞아서 안된다.
+		
+		
+	}
+	*/
+	@ResponseBody
+	@RequestMapping(value="ajax2.do",produces = "application/json; charset=UTF-8")
+	public String ajaxMethod2(int num) {
+		
+		//Member m = mService.selectMember(num); //일반적인 방법 but 지금 아무것도 없기 때문에 member안에 값이 담겨 있다고 가정
+		Member m = new Member("user01","pass01","차은우",20,"01011112222"); //더미(원래는 위의 코드 사용 해야 한는데...데이터가 없으니 가정해서 진행)
+		
+		return new Gson().toJson(m); //gson사용 하면 간단해짐 {userId:'user01', userPwd:'pass01',......}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="ajax3.do",produces = "application/json; charset=UTF-8")
+	public String ajaxMethod3() {
+		
+		//ArrayList<Member> list = mService.selectList(); =>원래 코드
+		ArrayList<Member> list = new ArrayList<Member>(); //[] =>처음 리스트 형태 
+		list.add(new Member("user01", "pass01", "차은우", 20, "01011111111")); //[{차은우 관련 객체}]
+		list.add(new Member("user02", "pass02", "주지훈", 30, "01011222222")); //[{차은우 관련 객체},{주지훈 관련 객체}]
+		list.add(new Member("user03", "pass03", "정우성", 40, "01033322222")); //[{차은우 관련 객체},{주지훈 관련 객체},{정우성 관련 객체}]
+		
+		return new Gson().toJson(list);
 	}
 	
 }
--- ajaxProject/src/main/webapp/index.jsp
+++ ajaxProject/src/main/webapp/index.jsp
@@ -30,6 +30,8 @@
 	/*data => 넘길값 (키:벨류 형식으로) 키=> 넘길때 이름, 벨류=> jsp에서 사용한 id값(.val() =>벨류값을 넘겨야 할때 여기서는 이름, 나이의 입력값을 넘겨야 하기 때문에 .val()사용  )
 	* 자바스크립트는 자료형 구분을 안해도 되기 때문에 매개변수 쓸때 자료형 신경 안쓰고 쓸수 있다.
 	*/
+	
+	/*
 		function test1(){
 			$.ajax({
 				url:"ajax1.do",
@@ -46,7 +48,116 @@
 				}
 			})
 		}
+*/
 
+			function test1(){
+				$.ajax({
+					url:"ajax1.do",
+					data:{
+						name:$("#name").val(),
+						age:$("#age").val()
+					},
+					success:function(result){
+						console.log(result);
+						
+						//응답데이터가 배열의 형태일 경우 => 인덱스에 접근 가능 =>  [인덱스]를 사용 
+						/*
+						let value = "이름 : " + result[0] + "<br> 나이 : " + result[1];
+						$("#result1").html(value);
+						*/
+						
+						//응답데이터가 단순객체의 형태일 경우 => 속성에 접근해야 한다. .속성명 => 속성명은 controller에서 사용한 키값
+						let value = "이름 : " + result.name + "<br> 나이 : " + result.age;
+						$("#result1").html(value);
+					},
+					error:function(){
+						console.log("ajax 통신 실패!!");
+					}
+				})
+			}
 	</script>
+	
+	<h3>2. 조회 요청 후 조회된 한 회원 객체를 응답 받아서 출력해보기</h3>
+	조회 할 회원번호 : <input type="number" id="userNo">
+	<button id="btn">조회</button>
+	
+	<div id="result2"></div>
+	
+	<script>
+		$(function(){
+			$("#btn").click(function(){
+				$.ajax({
+					url:"ajax2.do",
+					data:{num:$("#userNo").val()},
+					success:function(obj){
+						console.log(obj);
+						
+						/*객체 접근시 속성을 사용해야 하기 때문에 .을 사용해야 한다.*/
+						let value = "<ul>"
+									+ "<li>이름 : " + obj.userName + "</li>"
+									+ "<li>아이디 : " + obj.userId + "</li>"
+									+ "<li>나이 : " + obj.age + "</li>"
+									+"</ul>";
+					
+						$("#result2").html(value);
+						
+					},error:function(){
+						console.log("ajax 통신 실패");
+					}
+				})
+			})
+		})
+	</script>
+	
+	<h3>3. 조회요청 후 조회된 회원 리스트 응답받아서 출력해보기</h3>
+	<button onclick="test3();">회원 전체 조회</button>
+	<br><br>
+	
+	<table border="1" id="result3">
+		<thead>
+			<tr>
+				<th>아이디</th>
+				<th>이름</th>
+				<th>나이</th>
+				<th>전화번호</th>
+			</tr>
+		</thead>
+		<tbody>
+			
+		</tbody>
+	</table>
+	
+	<script>
+		function test3(){
+			$.ajax({
+				url:"ajax3.do",
+				success:function(list){
+					
+					console.log(list); //전부 조회
+					console.log(list[0]); //0번 인덱스에 해당되는 것 조회(차은우정보 조회)
+					console.log(list[0].userName); //0번째 인덱스에 있는 사람의 name속성 조회(차은우)
+					
+					let value = "";
+					for(let i in list){
+						value += "<tr>"
+								+"<td>" + list[i].userId + "</td>"
+								+"<td>" + list[i].userName + "</td>"
+								+"<td>" + list[i].age + "</td>"
+								+"<td>" + list[i].phone + "</td>"
+								+"</tr>";
+					}
+					
+					$("#result3 tbody").html(value);
+					
+				},error:function(){
+					console.log("ajax통신 실패");
+				}
+			})
+		}
+	</script>
+	
+	
+	
+	
 </body>
 </html>
